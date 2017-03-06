3e670d4c438d63f432bbce4f14ade58a44f04f0c
Dina <sergidka@gmail.com>
2017-03-06 13:44:22 +0300

    Add IRepository interface

---
+++ OnlineMarket.DataAccessLayer/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.DataAccessLayer.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IList<T> GetAll();
+
+        IList<T> Find(Func<T, bool> condition);
+
+        void Add(T item);
+
+        void Update(T item);
+
+        void Remove(T item);
+    }
+}
