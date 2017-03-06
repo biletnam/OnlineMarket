eaa4c32094d0d7ca46b096c82090376de1997664
Dina <sergidka@gmail.com>
2017-03-06 14:47:05 +0300

    Add IResourceService interface

---
+++ OnlineMarket.BusinessLogicLayer/Interfaces/IResourceService.cs
@@ -0,0 +1,12 @@
+﻿using OnlineMarket.DataAccessLayer.Entities;
+using System.Collections.Generic;
+
+namespace OnlineMarket.BusinessLogicLayer.Interfaces
+{
+    public interface IResourceService
+    {
+        IList<Resource> GetResources();
+
+        void ModifyResourcePrice(Resource resource, double price);
+    }
+}
