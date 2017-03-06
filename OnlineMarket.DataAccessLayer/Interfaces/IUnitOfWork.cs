1416e401c99b4de7b7e60a636d66027396561833
Dina <sergidka@gmail.com>
2017-03-06 14:08:38 +0300

    Add IUnitOfWork interface

---
+++ OnlineMarket.DataAccessLayer/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using OnlineMarket.DataAccessLayer.Entities;
+
+namespace OnlineMarket.DataAccessLayer.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Deal> DealRepository { get; }
+
+        IRepository<Resource> ResourceRepository { get; }
+
+        IRepository<User> UserRepository { get; }
+
+        void SaveChanges();
+    }
+}
