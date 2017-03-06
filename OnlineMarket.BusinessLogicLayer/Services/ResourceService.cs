150a76cba8d10f7014a962e131d2ac244443e1ec
Dina <sergidka@gmail.com>
2017-03-06 14:49:36 +0300

    Add ResourceService

---
+++ OnlineMarket.BusinessLogicLayer/Services/ResourceService.cs
@@ -0,0 +1,29 @@
+﻿using OnlineMarket.BusinessLogicLayer.Interfaces;
+using OnlineMarket.DataAccessLayer.Entities;
+using OnlineMarket.DataAccessLayer.Interfaces;
+using System.Collections.Generic;
+
+namespace OnlineMarket.BusinessLogicLayer.Services
+{
+    public class ResourceService : IResourceService
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public ResourceService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IList<Resource> GetResources()
+        {
+            return _unitOfWork.ResourceRepository.GetAll();
+        }
+
+        public void ModifyResourcePrice(Resource resource, double price)
+        {
+            resource.Price = price;
+            _unitOfWork.ResourceRepository.Update(resource);
+            _unitOfWork.SaveChanges();
+        }
+    }
+}
