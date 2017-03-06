305d899e03f84a8e7e3cda7ad9203b489c9381e3
Dina <sergidka@gmail.com>
2017-03-06 14:32:56 +0300

    Add IUserService interface

---
+++ OnlineMarket.BusinessLogicLayer/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using OnlineMarket.DataAccessLayer.Entities;
+using System.Collections.Generic;
+
+namespace OnlineMarket.BusinessLogicLayer.Interfaces
+{
+    public interface IUserService
+    {
+        IList<User> GetUsers();
+
+        void AddUser(User user);
+
+        void MoveUserToBannedGroup(User user);
+
+        void RemoveUser(User user);
+    }
+}
