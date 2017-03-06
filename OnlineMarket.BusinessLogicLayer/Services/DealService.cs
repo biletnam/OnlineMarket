ab1af07fe3f4b2b0d623df962b8675f9ff7ebcd7
Dina <sergidka@gmail.com>
2017-03-06 14:44:31 +0300

    Add DealService

---
+++ OnlineMarket.BusinessLogicLayer/Services/DealService.cs
@@ -0,0 +1,42 @@
+﻿using OnlineMarket.BusinessLogicLayer.Interfaces;
+using OnlineMarket.DataAccessLayer.Entities;
+using OnlineMarket.DataAccessLayer.Interfaces;
+using System.Collections.Generic;
+
+namespace OnlineMarket.BusinessLogicLayer.Services
+{
+    public class DealService : IDealService
+    {
+        private IUnitOfWork _unitOfWork;
+
+        private enum DealTypes { Purchase = 1, Sale}
+        public DealService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void AddPurchaseDeal(Deal deal)
+        {
+            deal.DealTypeId = (int)DealTypes.Purchase;
+            _unitOfWork.DealRepository.Add(deal);
+            _unitOfWork.SaveChanges();
+        }
+
+        public void AddSaleDeal(Deal deal)
+        {
+            deal.DealTypeId = (int)DealTypes.Sale;
+            _unitOfWork.DealRepository.Add(deal);
+            _unitOfWork.SaveChanges();
+        }
+
+        public IList<Deal> GetDeals()
+        {
+            return _unitOfWork.DealRepository.GetAll();
+        }
+
+        public IList<Deal> GetDealsByUserId(int userId)
+        {
+            return _unitOfWork.DealRepository.Find(d => d.UserId == userId);
+        }
+    }
+}
