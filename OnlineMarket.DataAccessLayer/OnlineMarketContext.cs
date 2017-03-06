239289d47584221f311eab829f315ab9a7867c79
Dina <sergidka@gmail.com>
2017-03-06 13:52:44 +0300

    Add UserRepository and DbContext

---
+++ OnlineMarket.DataAccessLayer/OnlineMarketContext.cs
@@ -0,0 +1,13 @@
+﻿using OnlineMarket.DataAccessLayer.Entities;
+using System.Data.Entity;
+
+namespace OnlineMarket.DataAccessLayer
+{
+    public class OnlineMarketContext : DbContext
+    {
+        public OnlineMarketContext() : base("OrderingFoodDB")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ OnlineMarket.DataAccessLayer/Repositories/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using OnlineMarket.DataAccessLayer.Entities;
+using OnlineMarket.DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace OnlineMarket.DataAccessLayer.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private OnlineMarketContext _context;
+
+        public UserRepository(OnlineMarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public IList<User> Find(Func<User, bool> condition)
+        {
+            return _context.Users.Where(condition).ToList();
+        }
+
+        public void Add(User item)
+        {
+            _context.Users.Add(item);
+        }
+
+        public void Update(User item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Remove(User item)
+        {
+            _context.Users.Remove(item);
+        }
+    }
+}
