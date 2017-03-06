371b51a0a8177501be4c9d408870e4d566e100f1
Dina <sergidka@gmail.com>
2017-03-06 14:40:39 +0300

    Add IDealService

---
+++ OnlineMarket.BusinessLogicLayer/Interfaces/IDealService.cs
@@ -0,0 +1,16 @@
+﻿using OnlineMarket.DataAccessLayer.Entities;
+using System.Collections.Generic;
+
+namespace OnlineMarket.BusinessLogicLayer.Interfaces
+{
+    public interface IDealService
+    {
+        IList<Deal> GetDeals();
+
+        IList<Deal> GetDealsByUserId(int userId);
+
+        void AddPurchaseDeal(Deal deal);
+
+        void AddSaleDeal(Deal deal);
+    }
+}
