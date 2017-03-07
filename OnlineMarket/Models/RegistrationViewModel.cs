2d6c0640741fe81c5db8dfde238fc47eed551bc8
Dina <sergidka@gmail.com>
2017-03-07 13:32:21 +0300

    Add RegistrationViewModel

---
+++ OnlineMarket/Models/RegistrationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineMarket.Models
+{
+    public class RegistrationViewModel
+    {
+        [Required]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+    }
+}
