be58c4ee6d1210c926be5f487bf4dcfb9b6c99f2
Dina <sergidka@gmail.com>
2017-03-10 17:11:56 +0300

    Fix auto mapper config

--- OnlineMarket/App_Start/AutoMapperConfig.cs
+++ OnlineMarket/App_Start/AutoMapperConfig.cs
@@ -14,10 +14,6 @@ namespace OnlineMarket
                 cfg.CreateMap<Resource, string>().ConstructUsing((resource) => resource.Title);
                 cfg.CreateMap<DealType, string>().ConstructUsing((dealtype) => dealtype.Type);
                 cfg.CreateMap<Deal, ArchiveViewModel>();
-            });
-
-            Mapper.Initialize(cfg =>
-            {
                 cfg.CreateMap<Role, string>().ConstructUsing((role) => role.Title);
                 cfg.CreateMap<User, UserViewModel>();
             });
